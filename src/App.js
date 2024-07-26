0bda1b520d736d4936e998064b377e5d7eddad1c
aleenaneo <aleena.george@neopraxisin>
2024-07-26 16:54:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'; // Must be imported for webpack to work
 import './App.css';
+
+
+import Header from 'remote/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+      <div className="container">Demo home page</div>
     </div>
   );
 }
