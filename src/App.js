381167a9987a625867423889971fcd8313e488f7
aleenaneo <aleena.george@neopraxisin>
2024-07-26 17:00:24 +0530

    updated app js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'; // Must be imported for webpack to work
+import React, { lazy, Suspense } from 'react'; // Must be imported for webpack to work
 import './App.css';
 
-
-import Header from 'remote/Header';
+const Header = lazy(() => import('HeaderApp/Header'));
 
 function App() {
   return (
     <div className="App">
+      <Suspense fallback={<div>Loading Header...</div>}>
         <Header />
+      </Suspense>
       <div className="container">Demo home page</div>
     </div>
   );
